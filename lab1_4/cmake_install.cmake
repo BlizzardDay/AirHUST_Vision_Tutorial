a0b7732690d05befb9a9671b7172a41bad37ac7b
BlizzardDay <921356927@example.com>
2026-02-02 23:49:05 +0800

    lab1_4 completed

--- lab1_4/cmake_install.cmake
+++ lab1_4/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/Git/AirHUST_Vision_Tutorial/lab1_4
+# Install script for directory: D:/Git/AirHUST_Vision_Tutorial/lab1_4
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "E:/Git/AirHUST_Vision_Tutorial/lab1_4/install_local_manifest.txt"
+  file(WRITE "D:/Git/AirHUST_Vision_Tutorial/lab1_4/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "E:/Git/AirHUST_Vision_Tutorial/lab1_4/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "D:/Git/AirHUST_Vision_Tutorial/lab1_4/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
